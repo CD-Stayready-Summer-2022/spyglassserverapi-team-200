cca4563aa7426c6fae24b58ab56b722d2b1b9a4c
RenatoRios1 <renatoarios1@gmail.com>
2022-08-03 13:29:12 -0500

    Project set up

---
+++ src/main/java/com/team200/spyglassserver/SpyglassServerApplication.java
@@ -0,0 +1,13 @@
+package com.team200.spyglassserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyglassServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyglassServerApplication.class, args);
+	}
+
+}
