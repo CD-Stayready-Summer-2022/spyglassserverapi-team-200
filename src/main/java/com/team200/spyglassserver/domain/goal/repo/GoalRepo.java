2a9ad0a2fdc07924a40c133fdcdb891635f9dc92
Boyce007 <diboyce21@students.desu.edu>
2022-08-03 15:37:13 -0400

    Goal and User Testing Packages

--- src/main/java/com/team200/spyglassserver/domain/goal/repo/GoalRepo.java
+++ src/main/java/com/team200/spyglassserver/domain/goal/repo/GoalRepo.java
@@ -1,19 +1,21 @@
 package com.team200.spyglassserver.domain.goal.repo;
 
+import com.team200.spyglassserver.domain.core.exceptions.enums.Status;
 import com.team200.spyglassserver.domain.goal.model.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
-public interface GoalRepo extends JpaRepository<Goal,Long> {
-    Goal findByTitle(String title);
+public interface GoalRepo extends JpaRepository<Goal, Long> {
+    Optional<Goal> findByTitle(String title);
 
     List<Goal> findByTargetDate(Date date);
 
     List<Goal> findByGoalDate(Date date);
 
-    List<Goal> findByStatus(Status status);
+    List<Goal>findByStatus(Status status);
 
-    
+
 }
--- src/main/java/com/team200/spyglassserver/domain/goal/services/GoalServiceImpl.java
+++ src/main/java/com/team200/spyglassserver/domain/goal/services/GoalServiceImpl.java
@@ -2,6 +2,7 @@ package com.team200.spyglassserver.domain.goal.services;
 
 import com.team200.spyglassserver.domain.core.exceptions.ResourceCreationException;
 import com.team200.spyglassserver.domain.core.exceptions.ResourceNotFoundException;
+import com.team200.spyglassserver.domain.core.exceptions.enums.Status;
 import com.team200.spyglassserver.domain.goal.model.Goal;
 import com.team200.spyglassserver.domain.goal.repo.GoalRepo;
 import com.team200.spyglassserver.domain.user.services.UserService;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class GoalServiceImpl implements GoalService {
@@ -24,7 +26,11 @@ public class GoalServiceImpl implements GoalService {
 
     @Override
     public Goal create(Goal goal) throws ResourceCreationException {
-        return null;
+        Optional<Goal> goalOptional  = goalRepo.findByTitle(goal.getTitle());
+        if(goalOptional.isPresent()) {
+            throw new ResourceCreationException();
+        }
+        return goalRepo.save(goal);
     }
 
     @Override
