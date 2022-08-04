17197af0fcc1b7fef58a2f5ba0c7dd17389008d9
davidupnext <dadeleke31@gmail.com>
2022-08-04 14:03:45 -0400

    Creation Test

--- src/test/java/com/team200/spyglassserver/com/team200/spyglassserver/domain/user/services/UserServiceTest.java
+++ src/test/java/com/team200/spyglassserver/com/team200/spyglassserver/domain/user/services/UserServiceTest.java
@@ -1,16 +1,24 @@
 package com.team200.spyglassserver.com.team200.spyglassserver.domain.user.services;
 
 import com.team200.spyglassserver.domain.user.dtos.UserCreateRequest;
+import com.team200.spyglassserver.domain.user.dtos.UserDTO;
 import com.team200.spyglassserver.domain.user.model.User;
 import com.team200.spyglassserver.domain.user.repo.UserRepo;
 import com.team200.spyglassserver.domain.user.services.UserService;
 import com.team200.spyglassserver.security.firebase.services.FirebaseUserMgrService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -30,5 +38,16 @@ public class UserServiceTest {
     @BeforeEach
     public void setup(){
         expectedId = "test237";
+        mockDetail = new UserCreateRequest("first", "last", "lastlast@gmail.com", "password");
+        mockUser = new User("first", "last", "lastlast@gmail.com");
+        mockUser.setId("test237");
+    }
+    @Test
+    public void userCreationTest01(){
+        BDDMockito.doReturn(Optional.empty()).when(userRepo).findByEmail(any());
+        BDDMockito.doReturn(expectedId).when(firebaseUserMgrService).createFireBaseUser(any());
+        BDDMockito.doReturn(mockUser).when(userRepo).save(any());
+        UserDTO user =userService.create(mockDetail);
+        Assertions.assertEquals(expectedId, user.getId());
     }
 }
